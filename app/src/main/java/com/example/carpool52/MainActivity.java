4a497474ceb8797912bb22a035eab4fb6a302b7d
ChristosKounatidis <kounax0@gmail.com>
2023-05-19 02:41:50 +0300

    UI fixes

--- app/src/main/java/com/example/carpool52/MainActivity.java
+++ app/src/main/java/com/example/carpool52/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.carpool52;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         db = FirebaseFirestore.getInstance();
 
